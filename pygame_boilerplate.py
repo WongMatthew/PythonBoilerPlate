d363b6e4f39814afdaa925c9e3aa4566b69c96b2
Matt Wong <49925170+WongMatthew@users.noreply.github.com>
2019-04-26 21:42:58 -0700

    Add files via upload

---
+++ pygame_boilerplate.py
@@ -0,0 +1,47 @@
+# pygame_boilerplate.py
+
+import pygame
+
+# ----- CONSTANTS
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+SKY_BLUE = (95, 165, 228)
+WIDTH = 800
+HEIGHT = 600
+TITLE = "<You're title here>"
+
+
+def main():
+    pygame.init()
+
+    # ----- SCREEN PROPERTIES
+    size = (WIDTH, HEIGHT)
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption(TITLE)
+
+    # ----- LOCAL VARIABLES
+    done = False
+    clock = pygame.time.Clock()
+
+    # ----- MAIN LOOP
+    while not done:
+        # -- Event Handler
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        # ----- LOGIC
+
+        # ----- DRAW
+        screen.fill(BLACK)
+
+        # ----- UPDATE
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
